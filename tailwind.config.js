9ecf1807f63f8dac4ce1614be5849f4b2759628f
Dmytro Karpenko <kardmitriy@gmail.com>
2023-09-15 22:42:49 +0300

    created

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          500: '#8A8A89',
+        },
+        black: '#121417',
+        white: '#F7F7FB',
+        blue: {
+          500: '#3470FF',
+        },
+        indigo: {
+          500: '#0B44CD',
+        },
+      },
+      fontFamily: {
+        sans: ['Manrope', 'Montserrat', 'sans-serif'],
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      },
+      spacing: {
+        // Визначте відступи (margins та paddings) для вашого проекту
+        1: '8px',
+        2: '16px',
+        3: '24px',
+        // Додайте інші значення відступів за потребою
+      },
+      // Додайте інші налаштування теми за потребою
+    },
+  },
+  plugins: ['prettier-plugin-tailwindcss'],
+};
